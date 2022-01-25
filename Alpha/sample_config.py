29f9cefcf3d78e98c668d0788740ca1a918623aa
Nethmal Prabashwara <96367042+neth1841@users.noreply.github.com>
2022-01-25 20:04:46 +0530

    Update sample_config.py

--- Alpha/sample_config.py
+++ Alpha/sample_config.py
@@ -4,7 +4,7 @@ import os
 
 
 def get_user_list(config, key):
-    with open("{}/Avatar/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/Aalpha/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
@@ -18,8 +18,8 @@ class Config(object):
     API_HASH = "API_HASH"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 123456  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "hirunaoficial"
-    SUPPORT_CHAT = "sltechzoneofficial"  # Your own group for support, do not add the @
+    OWNER_USERNAME = "Nethmal100 "
+    SUPPORT_CHAT = "TEAM_NBOT_GROUOP"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
         -1001253661229
     )  # Prints any new group the bot is added to, prints just the name and ID.
@@ -50,7 +50,7 @@ class Config(object):
     WOLVES = get_user_list("elevated_users.json", "whitelists")
     DONATION_LINK = None  # EG, paypal
     CERT_PATH = None
-    PORT = 5000
+    PORT = 8080
     DEL_CMDS = True  # Delete commands that users dont have access to, like delete /ban if a non admin uses it.
     STRICT_GBAN = True
     WORKERS = (

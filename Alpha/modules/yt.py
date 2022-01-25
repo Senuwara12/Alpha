adb4d0ca0a96fd7d892069d88fd92eea887c1971
Nethmal Prabashwara <96367042+neth1841@users.noreply.github.com>
2022-01-25 14:12:00 +0530

    Create yt.py

---
+++ Alpha/modules/yt.py
@@ -0,0 +1,10 @@
+﻿__help__ = """
+ - /yt <text>: perform a youtube search
+ - /ytaudio <link> or /ytvideo <link>: Downlods a video or audio from a youtube video to the bots local server
+ 
+Avatar
+"""
+__mod_name__ = "Youtube 🎞"
+© 2022 GitHub, Inc.
+Terms
+Privacy

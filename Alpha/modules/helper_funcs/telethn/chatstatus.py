aae7c41a86e14ff470efcafccfddd8a290a076cd
Nethmal Prabashwara <96367042+neth1841@users.noreply.github.com>
2022-01-26 18:04:59 +0530

    Update chatstatus.py

--- Alpha/modules/helper_funcs/telethn/chatstatus.py
+++ Alpha/modules/helper_funcs/telethn/chatstatus.py
@@ -1,7 +1,7 @@
 from telethon.tl.types import ChannelParticipantsAdmins
 
-from Avatar import DRAGONS
-from Avatar.modules.helper_funcs.telethn import IMMUNE_USERS, telethn
+from Aalha import DRAGONS
+from Alpha.modules.helper_funcs.telethn import IMMUNE_USERS, telethn
 
 
 async def user_is_ban_protected(user_id: int, message):
